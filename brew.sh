a35bbda85ae6b5f7670d1b005d68706f3467400b
Sean Stoops <brutimus@gmail.com>
2021-04-29 17:35:52 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -64,9 +64,10 @@ brew install vbindiff
 brew install zopfli
 
 # Install Applications
+brew install --cask google-backup-and-sync
 brew install --cask google-chrome
-brew install --cask slack
 brew install --cask iterm2
+brew install --cask slack
 brew install --cask sourcetree
 brew install --cask visual-studio-code
 
