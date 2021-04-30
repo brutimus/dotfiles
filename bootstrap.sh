af01a06091a41c386377f01a8760a4582db96435
Sean Stoops <brutimus@gmail.com>
2021-04-30 12:51:45 -0700

    Getting everything customized

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,26 +4,36 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
+files=(
+    ".aliases"
+    ".bash_profile"
+    ".bash_prompt"
+    ".bashrc"
+    ".curlrc"
+    ".editorconfig"
+    ".gitconfig"
+    ".gitignore"
+    ".p10k.zsh"
+    ".screenrc"
+    ".tmux.conf"
+    ".wgetrc"
+    ".zshrc"
+)
+
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "brew.sh" \
-		--exclude "init" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+    for i in "${files[@]}"
+    do
+        ln -s ".dotfiles/$i" ~
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This will create symlinks in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
