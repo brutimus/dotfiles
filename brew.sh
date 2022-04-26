4b58471bf44af601b0d3bb8e9cd45b21e1afea21
Sean <brutimus@gmail.com>
2022-04-26 14:22:14 -0700

    Update brew.sh
    Updates

--- brew.sh
+++ brew.sh
@@ -21,12 +21,12 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install a modern version of Bash.
 brew install bash
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
