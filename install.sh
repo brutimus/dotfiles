2285da675e7086b686f987e345cb3205d00cf789
Sean <brutimus@gmail.com>
2022-04-26 15:38:12 -0700

    Update install.sh
    p10k

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
